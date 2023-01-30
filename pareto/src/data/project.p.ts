3528cf705359f65c1c1733ee7a71f24418c3d361
corno <corno.astn.a@schraverus.com>
2023-01-30 21:39:54 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -12,19 +12,17 @@ export const $: mproject.TProject = {
     'author': "Corno",
     'description': "a pareto wrapper to create strings from complex types (dictionaries, arrays, nested types etcetera). Useful for (error) reporting. Should not be used in core functionality",
     'license': "ISC",
-
-    'type': ['resource',  {
-        'devDependencies': d({}),
-    }],
-    'modules': d({
-        "main": {
-            'definition': api,
-
-        },
-    }),
-    'main': "main",
+    
     'pubdependencies': d({
         "glo-pareto-common": {},
     }),
-    'testdependencies': d({}),
+    'type': ['resource', {
+        'definition': api,
+        'devDependencies': d({
+        }),
+        'test': {
+            'dependencies': d({
+            }),
+        }
+    }],
 }
