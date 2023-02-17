3f23b19871bcc399a387618a770967406333afae
corno <corno.astn.a@schraverus.com>
2023-02-17 15:28:40 +0100

    .

--- pareto/src/data/api.data.ts
+++ pareto/src/data/api.data.ts
@@ -12,9 +12,9 @@ import {
     type,
     optional,
     number,
-} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands.p"
+} from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
 
-import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands.p"
+import { definitionReference, constructor, algorithm } from "lib-pareto-typescript-project/dist/submodules/moduleDefinition/shorthands"
 
 import * as mmoduleDefinition from "lib-pareto-typescript-project/dist/submodules/moduleDefinition"
 const d = pd.wrapRawDictionary
